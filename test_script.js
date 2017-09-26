32422541d8c9bcc4d1b4e541fe73f28aa46c7836
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-26 12:40:26 -0600

    Got it to work with name data

--- test_script.js
+++ test_script.js
@@ -1,5 +1,8 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
+const name = process.argv.slice(2);
+
+console.log(name);
 
 const client = new pg.Client({
   user: settings.user,
@@ -14,12 +17,22 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * FROM famouse_people", ['Lincoln'], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    // console.log(result.rows[0].number); //output: 1
-    console.log(result);
-    client.end();
-  });
+  client.query("SELECT first_name, last_name, birthdate FROM famous_people WHERE last_name  IN ($1::text)",
+    name, (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      // console.log(result.rows[0].number); //output: 1
+      // console.log(result.rows[0]);
+      for (let x in result.rows[0]) {
+        console.log(result.rows[0][x]);
+        console.log(x);
+      }
+      // console.log(Object.keys(result.rows).length);
+      console.log(`Found ${result.rowCount} person(s) by the name '${name}:'`);
+      console.log(`-${result.rowCount}: ${result.rows[0].first_name} ${result.rows[0].last_name
+      }, born ${result.rows[0].birthdate.toISOString().substr(0, 10)}`);
+      // console.log(result.rows[0].first_name);
+      client.end();
+    });
 });
